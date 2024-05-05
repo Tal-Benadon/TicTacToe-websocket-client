7714f332d37db80873c7b2fd900d6d7b6d5856b8
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-05 12:57:38 +0300

    blabla

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import X from './componnents/X/index'
 import O from './componnents/O/index'
 import './index.css'
 import Button from './componnents/Button'
+import ButtonBack from './componnents/ButtonBack'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <> 
- <X/>
- <O/>
+<ButtonBack/>
  <Button/>
  </>
 )
