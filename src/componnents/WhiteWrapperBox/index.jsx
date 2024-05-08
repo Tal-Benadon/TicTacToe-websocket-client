006a335b4d4c00e042cef57cd5be614bc51063d2
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-08 10:25:05 +0300

    Witing page fixed

--- src/componnents/WhiteWrapperBox/index.jsx
+++ src/componnents/WhiteWrapperBox/index.jsx
@@ -14,14 +14,13 @@ export default function WhiteWrapperBox({ children, title = '', style = {} }) {
 
     return (
 
-
-        
         <div className={styles.wrapperContainer} style={{ ...style }}>
             {title ? <h2>{title}</h2> : ''}
+         
+            {children =
             <div className={styles.cod}>
             {newCod }
-            </div>
-            {children}
+            </div>}
         </div>
     )
 }
